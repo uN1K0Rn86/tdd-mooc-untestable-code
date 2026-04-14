b2a3f81825432b5b07643857ac816a2a02c1aee3
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-04-14 12:50:32 +0300

    Pass time as method parameter instead of setting inside method, write tests

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,21 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmas } from "../src/untestable1-copy.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
+  test("is an integer", () => {
     expect(daysUntilChristmas()).to.be.a("number");
+  });
+
+  test("is 1 on christmas eve", () => {
+    const now = new Date();
+    const christmasEve = new Date(now.getFullYear(), 11, 24);
+    expect(daysUntilChristmas(christmasEve)).to.be.equal(1);
+  });
+
+  test("is 30 on november 25", () => {
+    const now = new Date();
+    const nov25 = new Date(now.getFullYear(), 10, 25);
+    expect(daysUntilChristmas(nov25)).to.be.equal(30);
   });
 });
